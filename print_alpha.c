c453bc232a6ffa51afa1be68dbaab7536d3b77b5
k083k <kobekore@gmail.com>
2022-04-17 15:33:09 -0700

    handling printing string and percent sign

--- print_alpha.c
+++ print_alpha.c
@@ -15,3 +15,40 @@ int print_char(va_list arg)
 	return (1);
 }
 
+/**
+ * print_str - print a string
+ * @arg: argument of type va_list
+ * Return: len
+ */
+
+int print_str(va_list arg)
+{
+	int len = 0;
+	char *str;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_perc - print percent sign
+ * @arg: argument of time va_list
+ * Return: integer
+ */
+
+int print_perc(va_list arg)
+{
+	(void)arg;
+
+	_putchar('%');
+	return (1);
+}
