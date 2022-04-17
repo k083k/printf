22aabb55bb359013d1490fdf1042c7188defa22e
k083k <kobekore@gmail.com>
2022-04-17 15:54:22 -0700

    Header file with structures and prototypes

---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct printf - structure for printf
+ * @c: character
+ * @fun: function
+ */
+
+typedef struct printf
+{
+	char c;
+	int (*fun)(va_list);
+} printf_t;
+
+/* function prototypes */
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_perc(va_list arg);
+int print_integer(va_list arg);
+
+
+#endif
