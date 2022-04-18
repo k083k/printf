a239981a84ec4a1e41f6d88375e7b63443e22b9e
Klenam12 <klenamtsagli@gmial.com>
2022-04-18 01:45:28 -0700

    Handling Binary

--- print_nums.c
+++ print_nums.c
@@ -31,3 +31,34 @@ int print_integer(va_list arg)
 	}
 	return (count);
 }
+
+/**
+ * print_bin - print binary
+ * @arg: argument of type va_list
+ * Return: count
+ */
+
+int print_bin(va_list arg)
+{
+	unsigned int x, y, i, sum;
+	unsigned int a[32];
+	int count;
+	x = va_arg(arg, unsigned int);
+	y = 2147483648;
+	a[0] = x / y;
+	for (i = 1; i < 32; i++)
+	{
+		y = y / 2;
+		a[i] = (x / y) & 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum = sum +a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
